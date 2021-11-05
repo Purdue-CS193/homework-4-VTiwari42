22458de9ec02710dd9df581b768cfa5d244bb873
VTiwari42 <tiwari42@purdue.edu>
2021-11-05 08:19:48 -0400

    Changed Code to if else

--- src/Test1.java
+++ src/Test1.java
@@ -8,27 +8,23 @@ public class Test1 {
 
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
-
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
+        // if operation = ADD, return the sum
+        if (operation = Operation.ADD) {
                 result = num1 + num2;
-                break;
-            case SUBTRACT:
-                result = num1 - num2;
-                break;
-            case MULTIPLY:
-                result = num1 * num2;
-                break;
-            case DIVIDE:
-                if (num2 == 0) {
-                    System.out.println("Can't divide by 0");
-                } else {
-                    result = ((double)num1) / num2;
-                }
-                break;
+            }
+        else if (operation = Operation.SUBTRACT) {
+           return num1-num2;
         }
-        return result;
+        else if (operation = Operation.MULTIPLY) {
+            return num1*num2;
+        }
+        else if (operation = Operation.DIVIDE) {
+            if (num2 == 0) {
+                System.out.println("Can't divide by 0");
+            } else {
+                result = ((double)num1) / num2;
+            }
+        }
     }
 
     public static void main(final String[] args) {
